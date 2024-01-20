a10f629ed46b91eb3f3a0260f306fe816c60b8ee
Nordgaren <nordgarentv@gmail.com>
2024-01-19 19:43:21 -0700

    add commented out alternate way of doing things, to test if it's the project setup or not.

--- proc-macro-derive/src/lib.rs
+++ proc-macro-derive/src/lib.rs
@@ -3,9 +3,22 @@
 use proc_macro_core::proc_macro_impl;
 use proc_macro::TokenStream;
 use proc_macro_error::proc_macro_error;
+use quote::quote;
 
 #[proc_macro_error]
 #[proc_macro]
 pub fn demo_proc_macro(args: TokenStream) -> TokenStream {
     proc_macro_impl(args.into()).into()
+
+    // #[cfg(target_arch = "x86")]
+    // let q =quote!{
+    //     const TEST_STRING: &str = "Hello, fom x86_64!";
+    // };
+    //
+    // #[cfg(target_arch = "x86_64")]
+    // let q =quote!{
+    //     const TEST_STRING: &str = "Hello, fom x86_64!";
+    // };
+    //
+    // q.into()
 }
