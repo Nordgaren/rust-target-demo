fb03cdc9f3dfb426cfd0d39ed7b74f668733e86e
Nordgaren <nordgarentv@gmail.com>
2024-01-19 18:28:56 -0700

    made tests library for easier debugging

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 pub use proc_macro_derive::demo_proc_macro;
 
+#[cfg(test)]
+mod tests {
+    use proc_macro_derive::demo_proc_macro;
 
-#[test]
-fn import_test() {
-    demo_proc_macro!();
+    #[test]
+    fn import_test() {
+        demo_proc_macro!();
 
-    println!("{}", TEST_STRING);
+        println!("{}", TEST_STRING);
+    }
 }
